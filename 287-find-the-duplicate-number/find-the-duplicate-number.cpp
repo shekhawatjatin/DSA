ffde47d7813c69fb14968bd1fbcd1cd9d2f5f074
shekhawatjatin <jatinshekhawat2006@gmail.com>
2025-11-07 21:40:42 +0530

    Time: 29 ms (37.92%) | Memory: 65.1 MB (42.52%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        int low = 1;
+        int high = nums.size() - 1;
+        int duplicate = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                duplicate = mid;
+                high = mid - 1; 
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return duplicate;
+    }
+};
