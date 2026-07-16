9a4bd066313bc546c91244f8e1128e0f46282456
JATIN SINGH SHEKHAWAT <jatinshekhawat2006@gmail.com>
2026-07-16 21:09:19 +0530

    Time: 55 ms (98.31%) | Memory: 155.6 MB (87.10%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,39 +1,38 @@
+#include <vector>
+#include <algorithm>
+#include <numeric> 
+
+using namespace std;
+
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
-        int currentMax = 0; 
+        int currentMax = 0;
         vector<int> prefixGcd(n);
-
-   
+        
+        
         for (int i = 0; i < n; i++) {
             if (nums[i] > currentMax) {
                 currentMax = nums[i];
             }
-            prefixGcd[i] = gcd(nums[i], currentMax);
+            prefixGcd[i] = std::gcd(nums[i], currentMax);
         }
 
-
+       
         sort(prefixGcd.begin(), prefixGcd.end());
 
         int i = 0;
         int j = prefixGcd.size() - 1;
         long long sum = 0;
 
+
         while (i < j) {
-            sum += gcd(prefixGcd[i], prefixGcd[j]);
+            sum += std::gcd(prefixGcd[i], prefixGcd[j]);
             i++;
             j--;
         }
         
         return sum;
-    }
-    int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
     }
 };
